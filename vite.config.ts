fa7970163bd03c63f9188b4754d598c0e71c0be1
thayanesantos <tmsantos.web@gmail.com>
2025-04-11 22:55:26 -0300

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => ({
-  base: '/ts-harry-potter',
+  base: '/ts-harry-potter/',
   plugins: [react()],
 }))
